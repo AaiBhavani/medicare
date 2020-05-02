01549ed96466871ede011144cf711523cd433279
Coder Train <codertrain@gmail.com>
2020-05-02 15:27:11 +0530

    medicine AA model added

--- db/migrate/20200502094812_create_medicines.rb
+++ db/migrate/20200502094812_create_medicines.rb
@@ -3,12 +3,11 @@ class CreateMedicines < ActiveRecord::Migration[6.0]
     create_table :medicines do |t|
       t.string :name
       t.string :quantity
-      t.string :type
+      t.string :m_type
 
       t.timestamps
     end
     add_index :medicines, :name, unique: true
     add_index :medicines, :quantity, unique: true
-    add_index :medicines, :type, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_02_090828) do
+ActiveRecord::Schema.define(version: 2020_05_02_094812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,16 @@ ActiveRecord::Schema.define(version: 2020_05_02_090828) do
     t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
     t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+  end
+
+  create_table "medicines", force: :cascade do |t|
+    t.string "name"
+    t.string "quantity"
+    t.string "m_type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_medicines_on_name", unique: true
+    t.index ["quantity"], name: "index_medicines_on_quantity", unique: true
   end
 
   create_table "users", force: :cascade do |t|
